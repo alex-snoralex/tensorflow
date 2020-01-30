a391621870b4ff88afa819539687dad9f71c8e80
alex-snoralex <58669283+alex-snoralex@users.noreply.github.com>
2020-01-29 23:26:24 -0800

    Slight clean up

--- Celsius2Fahrenheit/BasicModel.py
+++ Celsius2Fahrenheit/BasicModel.py
@@ -10,8 +10,9 @@ logger.setLevel(logging.ERROR)
 
 celsius_q = np.array([-40, -10,  0,  8, 15, 22,  38],  dtype=float)
 fahrenheit_a = np.array([-40,  14, 32, 46, 59, 72, 100],  dtype=float)
-celsius_test = 100.0
+celsius_test = 100.00
 
+print("\nEntering test set: ")
 for i, c in enumerate(celsius_q):
     print("{} degrees Celsius = {} degrees Fahrenheit".format(c, fahrenheit_a[i]))
 
@@ -20,13 +21,13 @@ model = tf.keras.Sequential([l0])
 model.compile(loss='mean_squared_error',
               optimizer=tf.keras.optimizers.Adam(0.1))
 history = model.fit(celsius_q, fahrenheit_a, epochs=500, verbose=False)
-print("Finished training the model")
+print("Finished training the model\n")
 
 plt.xlabel('Epoch Number')
 plt.ylabel("Loss Magnitude")
 plt.plot(history.history['loss'])
 
-predicted = model.predict([celsius_test])
+predicted = round(float(model.predict([celsius_test])[0][0]), 2)
 print("For a celsius value of {}, the model predicted {} when the actual is {}."
       .format(celsius_test, predicted, celsius_2_fahrenheit(celsius_test)))
-print("These are the layer variables: {}".format(l0.get_weights()))
+print("Layer variables: {}".format(l0.get_weights()))
--- Celsius2Fahrenheit/Formula.py
+++ Celsius2Fahrenheit/Formula.py
@@ -1,2 +1,2 @@
 def celsius_2_fahrenheit(raw):
-    return (float(raw) * 1.8) + 32
+    return round((float(raw) * 1.8) + 32, 2)
