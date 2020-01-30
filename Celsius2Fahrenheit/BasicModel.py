33830bb1b7dd772a38ec671991e006a15d361715
Alex Padilla <alex_padilla@intuit.com>
2020-01-30 10:53:22 -0800

    Changed test set to be an array and updated prints

--- Celsius2Fahrenheit/BasicModel.py
+++ Celsius2Fahrenheit/BasicModel.py
@@ -8,26 +8,31 @@ import logging
 logger = tf.get_logger()
 logger.setLevel(logging.ERROR)
 
-celsius_q = np.array([-40, -10,  0,  8, 15, 22,  38],  dtype=float)
-fahrenheit_a = np.array([-40,  14, 32, 46, 59, 72, 100],  dtype=float)
-celsius_test = 100.00
+celsius_features = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
+fahrenheit_labels = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
+celsius_test_set = np.array([-56.2, -22.22, -3.1, 12.12, 45, 102, 453], dtype=float)
 
-print("\nEntering test set: ")
-for i, c in enumerate(celsius_q):
-    print("{} degrees Celsius = {} degrees Fahrenheit".format(c, fahrenheit_a[i]))
+print("\nEntering training set: ")
+for i, c in enumerate(celsius_features):
+    print("{} degrees Celsius = {} degrees Fahrenheit".format(c, fahrenheit_labels[i]))
 
+print("\nTraining the model")
 l0 = tf.keras.layers.Dense(units=1, input_shape=[1])
 model = tf.keras.Sequential([l0])
 model.compile(loss='mean_squared_error',
               optimizer=tf.keras.optimizers.Adam(0.1))
-history = model.fit(celsius_q, fahrenheit_a, epochs=500, verbose=False)
+history = model.fit(celsius_features, fahrenheit_labels, epochs=1000, verbose=False)
 print("Finished training the model\n")
+
+print("Beginning test set: ")
+for i, c in enumerate(celsius_test_set):
+    predicted = round(float(model.predict([celsius_test_set[i]])), 2)
+    print("{} degrees Celsius. {} Expected Fahrenheit. {} Predicted Fahrenheit."
+          .format(celsius_test_set[i], celsius_2_fahrenheit(c), predicted))
+
+print("Layer variables: {}".format(l0.get_weights()))
 
 plt.xlabel('Epoch Number')
 plt.ylabel("Loss Magnitude")
 plt.plot(history.history['loss'])
-
-predicted = round(float(model.predict([celsius_test])[0][0]), 2)
-print("For a celsius value of {}, the model predicted {} when the actual is {}."
-      .format(celsius_test, predicted, celsius_2_fahrenheit(celsius_test)))
-print("Layer variables: {}".format(l0.get_weights()))
+plt.show()
