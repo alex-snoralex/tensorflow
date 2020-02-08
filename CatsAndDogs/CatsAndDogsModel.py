250ccb65576297f6e2ae3071d430e73fafce4a54
alex-snoralex <58669283+alex-snoralex@users.noreply.github.com>
2020-02-08 00:08:19 -0800

    Fixing class and package names

--- CatsAndDogs/CatsAndDogsModel.py
+++ CatsAndDogs/CatsAndDogsModel.py
@@ -3,11 +3,9 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
-import matplotlib.pyplot as plt
-import numpy as np
 import logging
 
-from CatsVsDogs.ImageUtils import plot_images
+from CatsAndDogs.ImageUtils import plot_images
 
 logger = tf.get_logger()
 logger.setLevel(logging.ERROR)
