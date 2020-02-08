81324de7325b2422a2c4764d96e4182d59088390
alex-snoralex <alexpad107@gmail.com>
2020-02-07 20:34:36 -0800

    Started Cats vs Dogs package

---
+++ CatsVsDogs/CatsVsDogs.py
@@ -0,0 +1,73 @@
+# https://colab.research.google.com/github/tensorflow/examples/blob/master/courses/udacity_intro_to_tensorflow_for_deep_learning/l05c01_dogs_vs_cats_without_augmentation.ipynb
+
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import logging
+
+from CatsVsDogs.ImageUtils import plot_images
+
+logger = tf.get_logger()
+logger.setLevel(logging.ERROR)
+
+SHOW_TRAINING_AND_VALIDATION_INFO = True
+
+print("Downloading 'Cats & Dogs' dataset if not already present...")
+_URL = 'https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip'
+zip_dir = tf.keras.utils.get_file('cats_and_dogs_filterted.zip', origin=_URL, extract=True)
+
+base_dir = os.path.join(os.path.dirname(zip_dir), 'cats_and_dogs_filtered')
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+
+print("Configuring training and validation datasets...")
+train_cats_dir = os.path.join(train_dir, 'cats')  # directory with our training cat pictures
+train_dogs_dir = os.path.join(train_dir, 'dogs')  # directory with our training dog pictures
+validation_cats_dir = os.path.join(validation_dir, 'cats')  # directory with our validation cat pictures
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')  # directory with our validation dog pictures
+
+if SHOW_TRAINING_AND_VALIDATION_INFO:
+    num_cats_tr = len(os.listdir(train_cats_dir))
+    num_dogs_tr = len(os.listdir(train_dogs_dir))
+
+    num_cats_val = len(os.listdir(validation_cats_dir))
+    num_dogs_val = len(os.listdir(validation_dogs_dir))
+
+    total_train = num_cats_tr + num_dogs_tr
+    total_val = num_cats_val + num_dogs_val
+
+    print('total training cat images:', num_cats_tr)
+    print('total training dog images:', num_dogs_tr)
+
+    print('total validation cat images:', num_cats_val)
+    print('total validation dog images:', num_dogs_val)
+    print("--")
+    print("Total training images:", total_train)
+    print("Total validation images:", total_val)
+
+BATCH_SIZE = 100  # Number of training examples to process before updating our models variables
+IMG_SHAPE = 150  # Our training data consists of images with width of 150 pixels and height of 150 pixels
+
+train_image_generator = ImageDataGenerator(rescale=1./255)  # Generator for our training data
+validation_image_generator = ImageDataGenerator(rescale=1./255)  # Generator for our validation data
+
+train_data_gen = train_image_generator.flow_from_directory(batch_size=BATCH_SIZE,
+                                                           directory=train_dir,
+                                                           shuffle=True,
+                                                           target_size=(IMG_SHAPE, IMG_SHAPE),  # (150,150)
+                                                           class_mode='binary')
+
+val_data_gen = validation_image_generator.flow_from_directory(batch_size=BATCH_SIZE,
+                                                              directory=validation_dir,
+                                                              shuffle=False,
+                                                              target_size=(IMG_SHAPE, IMG_SHAPE),  # (150,150)
+                                                              class_mode='binary')
+
+if SHOW_TRAINING_AND_VALIDATION_INFO:
+    print("Plotting a small batch of training images...")
+    sample_training_images, _ = next(train_data_gen)
+    plot_images(sample_training_images[:5])  # Plot images 0-4
+
+print("Configuring the model...")
