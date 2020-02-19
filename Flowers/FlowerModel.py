faf9fecb131a46566af8fedc2ff637eb70f02f0f
alex-snoralex <alexpad107@gmail.com>
2020-02-18 18:48:32 -0800

    More FlowerModel.py edits, still unfinished

--- Flowers/FlowerModel.py
+++ Flowers/FlowerModel.py
@@ -9,6 +9,7 @@ import shutil
 import matplotlib.pyplot as plt
 from Flowers.ImageUtils import flower_classes, plot_images
 
+EPOCHS = 3  # 80 is recommended
 SHOW_PRE_TRAINING_INFO = True
 
 print("Downloading flower photos if not already present...")
@@ -44,31 +45,102 @@ IMG_SHAPE = 150
 
 if SHOW_PRE_TRAINING_INFO:
     print("Displaying horizontal flip")
-    image_gen = ImageDataGenerator(rescale=1. / 255, horizontal_flip=True)
-    train_data_gen = image_gen.flow_from_directory(batch_size=batch_size,
-                                                   directory=train_dir,
-                                                   target_size=(IMG_SHAPE, IMG_SHAPE),
-                                                   shuffle=True)
+    train_image_gen = ImageDataGenerator(rescale=1. / 255, horizontal_flip=True)
+    train_data_gen = train_image_gen.flow_from_directory(batch_size=batch_size,
+                                                         directory=train_dir,
+                                                         target_size=(IMG_SHAPE, IMG_SHAPE),
+                                                         shuffle=True)
     augmented_images = [train_data_gen[0][0][0] for i in range(5)]
     plot_images(augmented_images)
 
 if SHOW_PRE_TRAINING_INFO:
     print("Displaying rotation")
-    image_gen = ImageDataGenerator(rescale=1. / 255, rotation_range=45)
-    train_data_gen = image_gen.flow_from_directory(batch_size=batch_size,
-                                                   directory=train_dir,
-                                                   target_size=(IMG_SHAPE, IMG_SHAPE),
-                                                   shuffle=True)
+    train_image_gen = ImageDataGenerator(rescale=1. / 255, rotation_range=45)
+    train_data_gen = train_image_gen.flow_from_directory(batch_size=batch_size,
+                                                         directory=train_dir,
+                                                         target_size=(IMG_SHAPE, IMG_SHAPE),
+                                                         shuffle=True)
     augmented_images = [train_data_gen[0][0][0] for i in range(5)]
     plot_images(augmented_images)
 
 if SHOW_PRE_TRAINING_INFO:
     print("Displaying zoom")
-    image_gen = ImageDataGenerator(rescale=1. / 255, zoom_range=.5)
-    train_data_gen = image_gen.flow_from_directory(batch_size=batch_size,
-                                                   directory=train_dir,
-                                                   target_size=(IMG_SHAPE, IMG_SHAPE),
-                                                   shuffle=True)
+    train_image_gen = ImageDataGenerator(rescale=1. / 255, zoom_range=.5)
+    train_data_gen = train_image_gen.flow_from_directory(batch_size=batch_size,
+                                                         directory=train_dir,
+                                                         target_size=(IMG_SHAPE, IMG_SHAPE),
+                                                         shuffle=True)
     augmented_images = [train_data_gen[0][0][0] for i in range(5)]
     plot_images(augmented_images)
 
+train_image_gen = ImageDataGenerator(rescale=1. / 255,
+                                     horizontal_flip=True,
+                                     rotation_range=45,
+                                     zoom_range=.5)
+train_data_gen = train_image_gen.flow_from_directory(batch_size=batch_size,
+                                                     directory=train_dir,
+                                                     target_size=(IMG_SHAPE, IMG_SHAPE),
+                                                     shuffle=True)
+if SHOW_PRE_TRAINING_INFO:
+    print("Displaying all augmentations")
+    augmented_images = [train_data_gen[0][0][0] for i in range(5)]
+    plot_images(augmented_images)
+
+val_image_gen = ImageDataGenerator(rescale=1./255)
+val_data_gen = val_image_gen.flow_from_directory(batch_size=batch_size,
+                                                 directory=val_dir,
+                                                 target_size=(IMG_SHAPE, IMG_SHAPE),
+                                                 class_mode='sparse')
+
+print("Configuring model...")
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(5, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+print("Training model...")
+history = model.fit_generator(
+    train_data_gen,
+    steps_per_epoch=int(np.ceil(train_data_gen.__sizeof__() / float(batch_size))),
+    epochs=EPOCHS,
+    validation_data=val_data_gen,
+    validation_steps=int(np.ceil(val_data_gen.__sizeof__() / float(batch_size)))
+    )
+
+print("Training complete")
+
+acc = history.history['acc']  # tensorflow 1.0 = 'acc', 2.0 = 'accuracy'
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs_range = range(EPOCHS)
+
+print("\nPrinting training history...")
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.savefig('./super_training_vs_validation.png')
